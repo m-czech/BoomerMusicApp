1cc436c7e3e1b8c9459560da4a0a355e491aa9fc
matt <felvi00@gmail.com>
2022-06-06 14:07:38 +0200

    Implement console view for Song

---
+++ BoomerMusicApp/Views/ConsoleSongView.cs
@@ -0,0 +1,22 @@
+﻿using BoomerMusicApp.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoomerMusicApp.Views
+{
+    public class ConsoleSongView : IView<Song>
+    {
+        public void DisplayAll(List<Song> entities)
+        {
+            entities.ForEach(song => DisplaySingle(song));
+        }
+
+        public void DisplaySingle(Song entity)
+        {
+            Console.WriteLine($"Author: {entity.Author}, Title: {entity.Title}, Album name: {entity.AlbumName}, Lenght: {entity.Lenght}");
+        }
+    }
+}
---
+++ BoomerMusicApp/Views/IView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoomerMusicApp.Views
+{
+    public interface IView<T>
+    {
+        public void DisplaySingle(T entity);
+        public void DisplayAll(List<T> entities);
+    }
+}
