87eabdc9b5479bd1c52703a1a8811177b09e6e58
matt <felvi00@gmail.com>
2022-06-06 13:44:19 +0200

    Expand ConsoleInput with two more functions
    Expand ConsoleInput with:
    - FetchInputAsDoubleTruncatedToTwoDecimalPlaces(string),
    - FetchStringThatIsAtLeastOneLetterLong(string)

---
+++ BoomerMusicApp/Utils/UserInput/ConsoleInput.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoomerMusicApp.Utils
+{
+    public class ConsoleInput : IInput
+    {
+        public string FetchString(string prompt)
+        {
+            Console.WriteLine(prompt);
+            return Console.ReadLine();
+        }
+        public string FetchStringThatIsAtLeastOneLetterLong(string prompt)
+        {
+            var input = FetchString(prompt);
+            while (input.Length < 1)
+            {
+                input = FetchString(prompt);
+            }
+            return input;
+        }
+        public double FetchInputAsDoubleTruncatedToTwoDecimalPlaces(string prompt)
+        {
+            var input = FetchString(prompt);
+            double parsedInput;
+            while (!Double.TryParse(input, out parsedInput))
+            {
+                input = FetchString(prompt);
+            }
+            return parsedInput;
+        }
+
+
+    }
+}
---
+++ BoomerMusicApp/Utils/UserInput/IInput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoomerMusicApp.Utils
+{
+    public interface IInput
+    {
+        public string FetchString(string prompt);
+        public string FetchStringThatIsAtLeastOneLetterLong(string prompt);
+        public double FetchInputAsDoubleTruncatedToTwoDecimalPlaces(string prompt);
+
+
+    }
+}
