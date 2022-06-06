fb26a86cd7d239928878d23adb82d03750455dea
matt <felvi00@gmail.com>
2022-06-06 12:33:14 +0200

    Add Song entity model

---
+++ BoomerMusicApp.Data/Context/AppContext.cs
@@ -0,0 +1,21 @@
+﻿using BoomerMusicApp.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoomerMusicApp.Data.Context
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=OnlineStore;Integrated Security=True");
+        }
+
+    }
+}
---
+++ BoomerMusicApp.Data/Entities/Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoomerMusicApp.Data.Entities
+{
+    public class Song
+    {
+        public Guid SongId { get; set; }
+        public string Title { get; set; }
+        
+        public string Author { get; set; }
+        public string AlbumName { get; set; }
+        public double Lenght { get; set; }
+    }
+}
