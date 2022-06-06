75ff88bad46539f07dc2cd7073e0533a61f9b984
matt <felvi00@gmail.com>
2022-06-06 13:14:53 +0200

    Add SongHandler class template API

---
+++ BoomerMusicApp/Handlers/SongHandler.cs
@@ -0,0 +1,51 @@
+﻿using BoomerMusicApp.Data.DAL;
+using BoomerMusicApp.Data.Entities;
+using BoomerMusicApp.Utils;
+using BoomerMusicApp.Utils.Display;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoomerMusicApp.Handlers
+{
+    public class SongHandler
+    {
+        IRepository<Song> _songRepository;
+        IDisplay _display;
+        IInput _input;
+        public SongHandler(IInput input, IDisplay display)
+        {
+            _songRepository = new SongRepository();
+            _display = display;
+            _input = input;
+        }
+        public void AddNewSong()
+        {
+
+        }
+
+        public void DeleteExistingSong()
+        {
+
+        }
+
+        public void DisplaySpecificSong(Func<Song, bool> condition)
+        {
+
+        }
+
+        public void DisplayAllSongs()
+        {
+
+        }
+
+        public void DisplayAllSongsSortedByTitle()
+        {
+
+        }
+
+
+    }
+}
