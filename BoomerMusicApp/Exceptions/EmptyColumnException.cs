ddf35afbf5ae484468c97ebaf5967764929bb913
matt <felvi00@gmail.com>
2022-06-06 15:12:40 +0200

    Add custom EmptyColumnException

---
+++ BoomerMusicApp/Exceptions/EmptyColumnException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoomerMusicApp.Exceptions
+{
+    [Serializable]
+    public class EmptyColumnException : Exception
+    {
+        public EmptyColumnException() { }
+        public EmptyColumnException(string message) : base(message) { }
+
+        public EmptyColumnException(string message , Exception innerException) : base(message, innerException) { }
+    }
+}
