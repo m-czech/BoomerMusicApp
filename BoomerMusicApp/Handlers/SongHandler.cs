a685338f5bb5bbe65036bdab7e4097fbc96a1c71
matt <felvi00@gmail.com>
2022-06-06 14:14:46 +0200

    Implement SongHandler API
    User can now:
    - Remove existing song,
    - Display specific song,
    - Display all songs,
    - Display all songs sorted by title

--- BoomerMusicApp/Handlers/SongHandler.cs
+++ BoomerMusicApp/Handlers/SongHandler.cs
@@ -3,6 +3,7 @@ using BoomerMusicApp.Data.Entities;
 using BoomerMusicApp.Factories;
 using BoomerMusicApp.Utils;
 using BoomerMusicApp.Utils.Display;
+using BoomerMusicApp.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,37 +17,61 @@ namespace BoomerMusicApp.Handlers
         IRepository<Song> _songRepository;
         IDisplay _display;
         IInput _input;
-        public SongHandler(IInput input, IDisplay display)
+        IView<Song> _songView;
+        public SongHandler(IInput input, IDisplay display, IView<Song> songView)
         {
             _songRepository = new SongRepository();
             _display = display;
             _input = input;
+            _songView = songView;
         }
         public void AddNewSong()
         {
             var songFactory = new SongFactory(_input);
             var song = songFactory.Create();
             _songRepository.Add(song);
+            _songRepository.Save();
         }
 
         public void DeleteExistingSong()
         {
+            var songTitle = _input.FetchStringThatIsAtLeastOneLetterLong("Enter song's title: ");
+            var songAuthor = _input.FetchStringThatIsAtLeastOneLetterLong("Enter song's author: ");
+            var soughtSong = _songRepository.GetSingle(song => song.Title.Equals(songTitle) && song.Author.Equals(songAuthor));
+            // obsluga wyjatku zamiast sprawdzania nulla
+            if (soughtSong != null)
+            {
+                _songRepository.Delete(soughtSong);
+                _songRepository.Save();
+            }
+            else
+            {
+                _display.Display("Song does not exist in the database!");
+            }
 
         }
 
         public void DisplaySpecificSong()
         {
-
+            var songTitle = _input.FetchStringThatIsAtLeastOneLetterLong("Enter song's title: ");
+            var songAuthor = _input.FetchStringThatIsAtLeastOneLetterLong("Enter song's author: ");
+            var soughtSong = _songRepository.GetSingle(song => song.Title.Equals(songTitle) && song.Author.Equals(songAuthor));
+            
+            _songView.DisplaySingle(soughtSong);
         }
 
         public void DisplayAllSongs()
         {
-
+            _songView.DisplayAll(_songRepository.GetAll());
         }
 
         public void DisplayAllSongsSortedByTitle()
         {
-
+            var sortedSongs = _songRepository.GetAll().OrderBy(song => song.Title);
+            foreach (var song in sortedSongs)
+            {
+                _songView.DisplaySingle(song);
+            }
         }
 
 
