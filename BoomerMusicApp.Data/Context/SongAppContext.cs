161f35a0e62a1fc2291aac227e493362d216e43c
matt <felvi00@gmail.com>
2022-06-06 14:43:40 +0200

    Apply seed data to the database

--- BoomerMusicApp.Data/Context/SongAppContext.cs
+++ BoomerMusicApp.Data/Context/SongAppContext.cs
@@ -17,5 +17,14 @@ namespace BoomerMusicApp.Data.Context
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SongApp;Integrated Security=True");
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Song>().HasData(
+                new Song { AlbumName = "Lost & Found", Author = "Jorja Smith", Title = "Blue lights", Lenght = 4.10D },
+                new Song { AlbumName = "Best Things in Life", Author = "Dreamliners", Title = "Best things in life", Lenght = 2.23D },
+                new Song { AlbumName = "Nase male spiwanie", Author = "Siklawa", Title = "Takiego Janicka", Lenght = 3.43D }
+            );
+        }
+
     }
 }
