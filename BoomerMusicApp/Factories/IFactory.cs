1b1d117afee0b1b28cd014a71d2c1041ace4a606
matt <felvi00@gmail.com>
2022-06-06 13:53:20 +0200

    Add SongFactory

---
+++ BoomerMusicApp/Factories/IFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoomerMusicApp.Factories
+{
+    public interface IFactory<T> where T : class
+    {
+        public T Create();
+    }
+}
---
+++ BoomerMusicApp/Factories/SongFactory.cs
@@ -0,0 +1,29 @@
+﻿using BoomerMusicApp.Data.Entities;
+using BoomerMusicApp.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoomerMusicApp.Factories
+{
+    public class SongFactory : IFactory<Song>
+    {
+        IInput _input;
+        public SongFactory(IInput input)
+        {
+            _input = input;
+        }
+        public Song Create()
+        {
+            return new Song()
+            {
+                Author = _input.FetchStringThatIsAtLeastOneLetterLong("Enter author: "),
+                Title = _input.FetchStringThatIsAtLeastOneLetterLong("Enter title: "),
+                AlbumName = _input.FetchStringThatIsAtLeastOneLetterLong("Enter album name: "),
+                Lenght = _input.FetchInputAsNonNegativeDoubleTruncatedToTwoDecimalPlaces("Enter song length: ")
+            }
+        }
+    }
+}
